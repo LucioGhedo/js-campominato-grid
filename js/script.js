0a9bb05cb188900a8f66d26c143c66d245cfe238
LucioGhedo <lucioghedina@gmail.com>
2022-06-14 17:57:17 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 // CHIEDO DIFFICOLTA'
 let difficultyLevel = parseInt(prompt('Scegli la difficoltà; 1, 2 o 3'));
 let gameMaxRange = 0;
+
 // array vuoto per i numeri bomba
 let bombs = [];
+
+// creo una variabile che ferma il gioco in caso di sconfitta o vittoria
+let gamesContinue = true;
+
 // in base alla difficoltà genero le bombe
 if (difficultyLevel === 1) {
     gameMaxRange = 100;
     bombs = generateBombs(16, 1, 100)
+    gamesContinue = true;
 } else if (difficultyLevel === 2) {
     gameMaxRange = 81;
     bombs = generateBombs(16, 1, gameMaxRange)
+    gamesContinue = true;
 } else if (difficultyLevel === 3) {
     gameMaxRange = 49;
     bombs = generateBombs(16, 1, gameMaxRange)
+    gamesContinue = true;
+} else {
+    alert('refresh page e inserisci un numero valido')
+    gamesContinue = false;
 }
 // calcolo num massimo tentativi dell'utente per vincere la partita
 let maxAtt = gameMaxRange - 16
 
-// creo una variabile che ferma il gioco in caso di sconfitta o vittoria
-let gamesContinue = true;
 // creo array vuoto per i numeri dell utente
 let userScore = [];
 
